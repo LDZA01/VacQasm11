9b60c5325df24133f9240cd15540491712e9040f
Naphat Chartwanchai <naphatchartwanchai@gmail.com>
2024-03-02 17:34:48 +0700

    final version

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser=require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+
+const hospitals = require ('./routes/hospitals');
+const appointments = require('./routes/appointments');
+const auth = require('./routes/auth');
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+//load env vars
+dotenv.config({path:'./config/config.env'});
+
+//connect datebase
+connectDB();
+
+
+
+const app = express();
+//Body parser
+app.use(express.json());
+app.use(cookieParser());
+//Sanitize data
+app.use(mongoSanitize());
+//Set security headers
+app.use(helmet());
+//prevent xss attacks
+app.use(xss());
+//Rate Limiting
+const limiter = rateLimit({
+    windowMs:10*60*1000,//10 mins
+    max:1000
+});
+app.use(limiter);
+//Prevent http param pollutions
+app.use(hpp());
+//Enable CORS
+app.use(cors());
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments', appointments);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log('Sever running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));    
+})
